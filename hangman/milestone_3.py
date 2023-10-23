b629f0d999798007f7f81b4d897eff82abf9add9
Jawad El Amri <jawadelamri@jawads-air.home>
2023-10-24 00:12:22 +0100

    Adds functions to milestone_3

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -10,6 +10,8 @@ mystery_word = random.choice(word_list)
 print(mystery_word)
 
 def check_guess(player_guess):
+      """ This function checks if the player has guessed a correct letter from the mystery_word variable"""
+
       if player_guess.lower() in mystery_word:
                  print(f"Good guess! {player_guess} is in the word")
       else:
@@ -18,6 +20,11 @@ def check_guess(player_guess):
 
 
 def ask_for_input(): 
+    """ This function ensures the code runs continuously while the player enters their guess
+     
+      This is done using a while loop which is only borken if the correct conditions are met, I have also added an exception block. 
+       once the player has enter their guess correctly the previous check guess() function will run, checking if the letter is in 
+       the myster_word """
     while True:  
         try: 
             player_guess = input("Enter a single letter ")
