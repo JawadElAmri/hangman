f98d22ded6e12d21204d859ccbc311da5ab051b1
Jawad El Amri <jawadelamri@jawads-air.home>
2023-10-23 17:54:36 +0100

    Adds functions for milestone_3

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,18 +1,35 @@
-from milesto
-while True:  #loop to prevent code from stoppping
-    try: 
-        player_guess = input("Guess a single letter in the mystery word ")
-        if len(player_guess) == 1 and player_guess.isalpha() :     
-            # if length is equal to 1 and input is alpabetical letter loop will break
-            print("Good guess!")
-            break
-        else: 
-            print("Invalid letter. Please Enter a single alphabetical character.")
-    except Exception as e: #catch and print exceptions 
-        print(e) 
+import random 
+#adding random package
+
+word_list = ["apple", "plum", "grape", "orange", "apricot"] 
+#creating list of 5 fruits to be guessed
+print(word_list)
+#showing list in terminal 
+mystery_word = random.choice(word_list)
+#choosing a random word from the list
+print(mystery_word)
+
+def check_guess(player_guess):
+      if player_guess.lower() in mystery_word:
+                 print(f"Good guess! {player_guess} is in the word")
+      else:
+            print(f"Sorry {player_guess} is not in the word. Try again.")
 
 
-if player_guess.lower() in mystery_word:
-    print("Good guess! f'{player_guess}' is in the word")
-else:
-    print("Sorry f'{player_guess}' is not in the word. Try again.")x
+
+def ask_for_input(): 
+    while True:  
+        try: 
+            player_guess = input("Enter a single letter ")
+            if len(player_guess) == 1 and player_guess.isalpha():
+                  print("Good guess!")
+                  break
+            else: 
+                  print("Oops! That is not a vaild input.")
+        except Exception as e: 
+              print(e) 
+    
+    check_guess(player_guess)
+
+ask_for_input()
+
